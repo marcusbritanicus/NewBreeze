6fcbd5a92cd884c0fd6d0fb1e4e44494a2cfdbfe
Marcus Britanicus <marcusbritanicus@gmail.com>
2020-09-01 19:00:45 +0530

    .gitlab-ci.yml qtermwidget bugfix

--- app/Gui/Dialogs/PropertiesDialog/NBPropsWidget.cpp
+++ app/Gui/Dialogs/PropertiesDialog/NBPropsWidget.cpp
@@ -280,10 +280,14 @@ void NBPropertiesWidget::changeDirIcon() {
 	);
 
 	if ( not iconName.isEmpty() ) {
+		qDebug() << "Setting new icon for the folder" << pathsList.at( 0 );
 		iconLbl->setPixmap( icon( QStringList() << iconName ).pixmap( 36 ) );
 
+		qDebug() << "Saving the new icon for the folder" << pathsList.at( 0 );
 		QSettings dSett( pathsList.at( 0 ) + "/.directory", QSettings::NativeFormat );
 		dSett.setValue( "NewBreeze/Icon", iconName );
+
+		qDebug() << "Syncing the changes";
 		dSett.sync();
 	}
 };
